ac8131ebbce56e28fd815895f0b0853c49bd1e23
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2019-01-17 12:52:25 -0200

    Add simple quickstart maven project

---
+++ src/main/java/com/itquasar/multiverse/jagrosk/App.java
@@ -0,0 +1,13 @@
+package com.itquasar.multiverse.jagrosk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/itquasar/multiverse/jagrosk/AppTest.java
@@ -0,0 +1,20 @@
+package com.itquasar.multiverse.jagrosk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
